fd8468444038b7df9c5e6e15606eee6737d27eb6
Yash <ya45242@gmail.com>
2024-02-09 09:29:28 -0800

    styling like button and adding some javascript

--- script.js
+++ script.js
@@ -9,3 +9,11 @@ function bgbox1(){
     var box1 = document.getElementById('box1');
     box1.style.backgroundColor = "rgb(255, 0, 0)";
 }
+function bgbox2(){
+    var box2 = document.getElementById('box2');
+    box2.style.backgroundColor = "rgb(255, 0, 0)";
+}
+function bgbox3(){
+    var box3 = document.getElementById('box3');
+    box3.style.backgroundColor = "rgb(255, 0, 0)";
+}
