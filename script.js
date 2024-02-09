2cfa3b6b15966dac0d0376aba2b7f3c80b396e78
Yash <ya45242@gmail.com>
2024-02-09 10:17:11 -0800

    adding functions in save button for downloading of video

--- script.js
+++ script.js
@@ -17,3 +17,33 @@ function bgbox3(){
     var box3 = document.getElementById('box3');
     box3.style.backgroundColor = "rgb(255, 0, 0)";
 }
+function saveVideo2(){
+    var video = document.getElementById('myVideo2');
+    var url = video.src;
+    var a = document.createElement('a');
+    a.href = url;
+    a.download = 'DARE_edit.mp4';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
+function saveVideo1(){
+    var video = document.getElementById('myVideo1');
+    var url = video.src;
+    var a = document.createElement('a');
+    a.href = url;
+    a.download = 'chal.mp4';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
+function saveVideo3(){
+    var video = document.getElementById('myVideo3');
+    var url = video.src;
+    var a = document.createElement('a');
+    a.href = url;
+    a.download = 'WHY_Trim.mp4';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
